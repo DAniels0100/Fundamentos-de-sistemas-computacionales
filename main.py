6532dd9c88835b47e09d25b6e7164a0bb8b518b9
Daniels <se.alchanto@gmail.com>
2024-05-09 20:36:02 -0600

    third commit

--- main.py
+++ main.py
@@ -5,25 +5,26 @@ import sys
 import _thread
 
 
-pin_A = machine.Pin(18, machine.Pin.OUT)  # Pin A y B del registro (conectado a GPIO 3)
-pin_CLK = machine.Pin(19, machine.Pin.OUT)  # Pin CLK del registro (conectado a GPIO 2)
+pin_A = machine.Pin(18, machine.Pin.OUT)  #pin A y B del registro de corrimiento
+pin_CLK = machine.Pin(19, machine.Pin.OUT)  #pin clock del registro de corrimiento
 ledTorre1 = machine.Pin(26, machine.Pin.OUT)
 ledTorre2 = machine.Pin(22, machine.Pin.OUT)
 potenciometro = ADC(27)
-boton= machine.Pin(5, machine.Pin.IN, Pin.PULL_DOWN)
+boton= machine.Pin(4, machine.Pin.IN, Pin.PULL_DOWN)
 
-paleta1=machine.Pin(12, machine.Pin.IN)
-paleta2=machine.Pin(11, machine.Pin.IN)
-paleta3=machine.Pin(10, machine.Pin.IN)
-paleta4=machine.Pin(9, machine.Pin.IN)
-paleta5=machine.Pin(8, machine.Pin.IN)
-paleta6=machine.Pin(7, machine.Pin.IN)
+#pines de las paletas
+paleta1 = Pin(9, Pin.IN)
+paleta2=machine.Pin(8, machine.Pin.IN)
+paleta3=machine.Pin(7, machine.Pin.IN)
+paleta4=machine.Pin(12, machine.Pin.IN)
+paleta5=machine.Pin(11, machine.Pin.IN)
+paleta6=machine.Pin(10, machine.Pin.IN)
 
 
-visitante=False
-local=False
- # Config de los pines de entrada  
 
+
+
+#prender los led segun la configuracion del portero
 def turnOnLed(status_led):
     pin_A.value(0)
     for led in status_led:
@@ -32,16 +33,21 @@ def turnOnLed(status_led):
         pin_A.value(led)
         pin_CLK.value(1)
     
-
+#variables generales
 eleccion=randint(0,6)
 apagarLed=[0,0,0,0,0,0]
+prenderLed=[1,1,1,1,1,1]
 estadosChancha=[[1,1,0,0,0,0], [0,0,1,1,0,0], [0,0,0,0,1,1],[1,1,1,0,0,0], [0,0,0,1,1,1], [1,0,1,0,1,0], [0,1,0,1,0,1]]
+
+contadorTirosHechos=0
 
 run=True
 while run:
+    #print("contador", contadorTirosHechos)
+    
+    
     valorPotenciometro = potenciometro.read_u16()
     time.sleep(2)
-    numero=1
     
     valorPotenciometroStr= str(valorPotenciometro)
     sys.stdout.write("#"+valorPotenciometroStr+'\n')
@@ -49,68 +55,39 @@ while run:
     botonStr=str(boton.value())
     sys.stdout.write("%"+botonStr+'\n')
     
-    print('1 '+str(paleta1.value()))
-    print("2 "+str(paleta2.value()))
-    print("3 "+str(paleta3.value()))
-    print("4 "+str(paleta4.value()))
-    print("5 "+str(paleta5.value()))
-    print("6 "+str(paleta6.value()))
     
-    if numero:
-        eleccion=randint(0,6)
-        turnOnLed(estadosChancha[eleccion])
+    eleccion=0#randint(0,6)
+    turnOnLed(estadosChancha[eleccion])
 
-        if paleta1.value()==0 and estadosChancha[eleccion][0]==1:
-            sys.stdout.write("A")
-            
-        if paleta2.value()==0 and estadosChancha[eleccion][1]==1:
-           sys.stdout.write("B")
-           
-        if paleta3.value()==0 and estadosChancha[eleccion][2]==1:
-            sys.stdout.write("C")
-            
-        if paleta4.value()==0 and estadosChancha[eleccion][3]==1:
-            sys.stdout.write("D")
-            
-        if paleta5.value()==0 and estadosChancha[eleccion][4]==1:
-            sys.stdout.write("E")
-            
-        if paleta6.value()==0 and estadosChancha[eleccion][5]==1:
-            sys.stdout.write("F")
+    if paleta1.value()==0 and estadosChancha[eleccion][0]==1:
+        sys.stdout.write("A")
+        
+    if paleta2.value()==0 and estadosChancha[eleccion][1]==1:
+       sys.stdout.write("B")
+       
+    if paleta3.value()==0 and estadosChancha[eleccion][2]==1:
+        sys.stdout.write("C")
+        
+    if paleta4.value()==0 and estadosChancha[eleccion][3]==1:
+        sys.stdout.write("D")
+        
+    if paleta5.value()==0 and estadosChancha[eleccion][4]==1:
+        sys.stdout.write("E")
+        
+    if paleta6.value()==0 and estadosChancha[eleccion][5]==1:
+        sys.stdout.write("F")
 
-    if numero==0:
-        turnOnLed(apagarLed)
-    visitante, local= True, True
-    if visitante:
+    if boton.value():
+        contadorTirosHechos+=1
+    
+    if contadorTirosHechos%2==0:
         ledTorre1.value(1)
-        
-        
-        #visitante=False
-        
-    if local:
+        ledTorre2.value(0)
+    else:
+        ledTorre1.value(0)
         ledTorre2.value(1)
         
-        
-        #local=False
-        #print(local)
-    
-    #if valor_potenciometro ==0:
- 
-     #   turnOnLed(estadosCancha[0])
-    #elif valor_potenciometro >50000:
-     #   lista_led=[0,0,0,1,1,0]
-      #  turnOnLed(lista_led)
-    #elif valor_potenciometro <50000 and valor_potenciometro >40000:
-        
-        #lista_led=[0,1,1,1,0,0]
-        #turnOnLed(lista_led)
-        
-    #elif numero == 4:
-        
-        #lista_led=[0,1,1,1,1,0]
-        #turnOnLed(lista_led)
-        
+
+
 
 pin_A.value(0)
-
-
---
+++ tests.py
@@ -0,0 +1,65 @@
+import pygame
+
+import serial
+
+pygame.init()
+
+screen=pygame.display.set_mode((800,600))
+
+screen.fill((25,255,255))
+Rpi=serial.Serial(port="COM4", baudrate=115200)
+
+try:
+    Rpi.open()
+except:
+    if (Rpi.isOpen()):
+        print("conectado")
+    else:
+        print('no conecto')
+
+
+run=True
+while run:
+
+    if (Rpi.isOpen()):
+        datos=Rpi.readline()
+        decode=datos.decode('UTF-8')
+        if decode[0]=="#":
+            decode=decode[1:]
+            if int(decode)<=5:
+                print("equipo 1",decode)
+            elif int(decode)>5 and int(decode)<=10:
+                print("equipo 2", decode)
+            elif int(decode)>10:
+                print('equipo 3', decode)
+        if decode[0]=="%":
+            decode=decode[1:]
+            if int(decode)==1:
+                print("selecionado")
+        if decode[0]=="A":
+            print('paleta 1 activa')
+            screen.fill((100,100,200))
+        if decode[0]=="B":
+            print('paleta 2 activa')
+            screen.fill((100,200,100))
+        if decode[0]=="C":
+            print('paleta 3 activa')
+            screen.fill((200,100,100))
+        if decode[0]=="D":
+            print('paleta 4 activa')
+            screen.fill((150,100,100))
+        if decode[0]=="E":
+            print('paleta 5 activa')
+            screen.fill((100,100,150))
+        if decode[0]=="F":
+            print('paleta 6 activa')
+            screen.fill((100,150,100))
+
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            Rpi.close()
+            quit()
+    pygame.display.update()
